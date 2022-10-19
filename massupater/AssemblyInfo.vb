630f8884b40251def2570bd1546bc339c669740e
Jamie Smith <51212403+smithjb84@users.noreply.github.com>
2022-10-19 20:39:38 +0100

    Add files via upload

---
+++ massupater/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("massupater")>
+<Assembly: AssemblyDescription("massupater")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d74aa6cb-d26e-42e5-b246-31792414f1de")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")>
---
+++ massupater/CommandFunctions.vb
@@ -0,0 +1,30 @@
+﻿' TODO: This module exists as a convenient location for the code that does the real
+'       work when a command is executed.  If you're converting VBA macros into add-in 
+'       commands you can copy the macros here, make changes to make them VB.NET compatible, 
+'       and change any references to "ThisApplication" to "g_inventorApplication".
+
+Public Module CommandFunctions
+	' Function that's called when the button is clicked.
+	Public Sub SampleCommandFunction()
+		Dim oDoc As Inventor.Document
+		oDoc = g_inventorApplication.ActiveDocument
+
+		If oDoc.DocumentType = Inventor.DocumentTypeEnum.kDrawingDocumentObject Then
+
+			Dim oSheet As Inventor.Sheet
+			oSheet = oDoc.ActiveSheet
+
+			Dim oDrawingView As Inventor.DrawingView
+			Dim oModel As Inventor.Document
+			Dim mass As Double
+
+			If oSheet.DrawingViews.Count <> 0 Then
+				oDrawingView = oSheet.DrawingViews.Item(1)
+				oModel = oDrawingView.ReferencedDocumentDescriptor.ReferencedDocument
+				mass = oModel.ComponentDefinition.MassProperties.Mass
+				oDoc.Update()
+			End If
+
+		End If
+	End Sub
+End Module
